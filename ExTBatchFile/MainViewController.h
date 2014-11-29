ff8aed4942df5809c3f67c97869c616d03e35a6d
onikurchan <onikurchan@gmail.com>
2014-11-29 21:02:00 +0800

    Update Licenses to GNU GPL v3.0

--- ExTBatchFile/MainViewController.h
+++ ExTBatchFile/MainViewController.h
@@ -5,6 +5,20 @@
 //  Created by Onikur Chan on 29/11/14.
 //  Copyright (c) 2014 TANPN. All rights reserved.
 //
+//  This file is part of ExTBatchFile.
+//
+//  ExTBatchFile is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ExTBatchFile is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ExTBatchFile.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Cocoa/Cocoa.h>
 #import "OptionView.h"
